d31a10cb52570a576128cc77b1a39d5f26b6464b
tbnsok40 <tbnsok40@naver.com>
2021-03-27 17:55:37 +0900

    [test] 모듈 구조 변경

--- app/core/models.py
+++ app/core/models.py
@@ -3,9 +3,10 @@ from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, \
     PermissionsMixin
 
 
+# implement custom user model
+# this class is helper class for providing helper functions for creating a user or creating a super user.
 class UserManager(BaseUserManager):  # to override some methods
     def create_user(self, email, password=None, **extra_fields):  # **extra_fields: makes function more flexible
-
         if not email:
             raise ValueError('Users must have an email address')
 
@@ -22,6 +23,8 @@ class UserManager(BaseUserManager):  # to override some methods
         user.save(using=self._db)
         return user
 
+
+# functions 는 없고 fields 만 존재
 class User(AbstractBaseUser, PermissionsMixin):
     """custom user model that supports using email instead of username"""
     email = models.EmailField(max_length=255, unique=True)
@@ -29,6 +32,8 @@ class User(AbstractBaseUser, PermissionsMixin):
     is_active = models.BooleanField(default=True)
     is_staff = models.BooleanField(default=False)
 
-    objects = UserManager()
+    # 이걸로 인하여, 상위의 UserManager 클래스를 받아온 것 --> 왜 굳이 UserManager()를 만드는 거야?
+    # 현재 클래스는 변수만 선언하고, 함수는 상위 클래스에서 받아오도록 분리한
+    objects_ = UserManager()
 
     USERNAME_FIELD = 'email'
--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -14,7 +14,7 @@ class ModelTests(TestCase):
         password = 'testpassword123'
 
         # create_user()메서드는 models.py에서 가져온 것..? ㄴㄴ get_user_model()에서 가져옴
-        user = get_user_model().objects.create_user(
+        user = get_user_model().objects_.create_user(
             email=email,
             password=password
         )
@@ -23,17 +23,19 @@ class ModelTests(TestCase):
 
     def test_new_user_email_normalized(self):
         email = 'test@LONDON.COM'
-        user = get_user_model().objects.create_user(email, 'test123')
+        user = get_user_model().objects_.create_user(email, 'test123')
         self.assertEqual(user.email, email.lower())  # 양쪽 모두 소문자여야 test success
 
     def test_new_user_invalid_email(self):
         with self.assertRaises(ValueError):  # valueError가 일어나지 않으면 fail 한다.
-            get_user_model().objects.create_user(None, 'test123')
+            get_user_model().objects_.create_user(None, 'test123')
 
     def test_create_new_superuser(self):
-        user = get_user_model().objects.create_superuser(
+        user = get_user_model().objects_.create_superuser(
             'test@name.com',
             'test123'
         )
         self.assertTrue(user.is_superuser)
         self.assertTrue(user.is_staff)
+# settings.py에서 AUTH_USER_MODEL = "core.User" 이렇게 설정을 해뒀기 때문에,
+# get_user_model().objects_는 models.py의 class User()로 부터 온 것
